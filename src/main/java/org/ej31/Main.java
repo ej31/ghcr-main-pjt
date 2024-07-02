5655cb5034240f1568a555a6e93f0928d847cbe8
ej31 <devil1032@gmail.com>
2024-07-03 03:06:55 +0900

    add: initialized

---
+++ src/main/java/org/ej31/Main.java
@@ -0,0 +1,10 @@
+package org.ej31;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet("ej31"));
+
+    }
+}
